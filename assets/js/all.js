132cd7796dfc11beb9418784df526b689b73cde9
dean <dean19990226@gmail.com>
2022-09-12 15:46:09 +0800

    Update 2022-09-12T07:46:06.505Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -20,7 +20,7 @@ AOS.init({
   throttleDelay: 99,
   // the delay on throttle used while scrolling the page (advanced)
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120,
+  offset: 110,
   // offset (in px) from the original trigger point
   delay: 0,
   // values from 0 to 3000, with step 50ms
