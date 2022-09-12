ba4b3156d5b16daa946dcd0fbcf8f623c70e5e12
dean <dean19990226@gmail.com>
2022-09-12 14:43:09 +0800

    Update 2022-09-12T06:43:04.696Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -5,8 +5,8 @@ AOS.init({
   // Global settings:
   disable: false,
   // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded',
-  // name of the event dispatched on the document, that AOS should initialize on
+  //startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  startEvent: 'load',
   initClassName: 'aos-init',
   // class applied after initialization
   animatedClassName: 'aos-animate',
@@ -20,7 +20,7 @@ AOS.init({
   throttleDelay: 99,
   // the delay on throttle used while scrolling the page (advanced)
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 150,
+  offset: 120,
   // offset (in px) from the original trigger point
   delay: 0,
   // values from 0 to 3000, with step 50ms
@@ -34,6 +34,9 @@ AOS.init({
   // whether elements should animate out while scrolling past them
   anchorPlacement: 'top-bottom' // defines which position of the element regarding to window should trigger the animation
 
+});
+window.addEventListener('load', function () {
+  AOS.refresh();
 }); // loading 動畫start
 
 $(window).on('load', function () {
